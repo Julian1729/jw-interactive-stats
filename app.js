ba1739c3c91290daf243e87cea063bbaf934bb39
Julian1729 <hernandez.julian17@gmail.com>
2020-11-09 21:30:07 -0500

    rebuild again

--- app.js
+++ app.js
@@ -25,8 +25,6 @@ app.use(express.static(path.join(__dirname, "build")));
 // });
 
 app.use(express.static(path.join(__dirname, "client/build")));
-// rebiuld
-//
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
