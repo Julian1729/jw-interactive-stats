45c53ef378602052bb0f56f729ec16ead73bf195
Julian1729 <hernandez.julian17@gmail.com>
2020-11-14 17:10:26 -0500

    Add animation and more responsive styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 import _ from "lodash";
 import CountUp from "react-countup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faTimesCircle, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 // import countries from "../countries.json";
 import { CountryContext } from "./CountryProvider";
 
 const Sidebar = () => {
-  const [country] = useContext(CountryContext);
+  const [country, setCountry] = useContext(CountryContext);
 
   return (
     <aside className="sidebar">
@@ -21,12 +21,15 @@ const Sidebar = () => {
 
       {!_.isEmpty(country) && (
         <>
-          <h2 className="sidebar__country-name">
+          <h2
+            className={`sidebar__country-name ${!country.stats && 'sidebar__country-name--nostats'}`}
+            onClick={() => setCountry({})}
+            >
             {country.name}{" "}
             {country.stats && (
               <FontAwesomeIcon
-                className="sidebar__mobile-arrow"
-                icon={faChevronDown}
+                className="sidebar__reset-icon"
+                icon={faTimesCircle}
               />
             )}
           </h2>
@@ -35,13 +38,14 @@ const Sidebar = () => {
               <ul className="sidebar__stats">
                 <li className="sidebar__ministers">
                   <span className="sidebar__stat-value">
-                    <CountUp separator={","} end={country.stats.ministers} />
+                    <CountUp delay={0.30} separator={","} end={country.stats.ministers} />
                   </span>
                   <span className="sidebar__stat-title">Ministers</span>
                 </li>
                 <li className="sidebar__congregations">
                   <span className="sidebar__stat-value">
                     <CountUp
+                      delay={0.60}
                       separator={","}
                       end={country.stats.congregations}
                     />
@@ -50,13 +54,13 @@ const Sidebar = () => {
                 </li>
                 <li className="sidebar__populations">
                   <span className="sidebar__stat-value">
-                    <CountUp separator={","} end={country.stats.population} />
+                    <CountUp delay={0.90} separator={","} end={country.stats.population} />
                   </span>
                   <span className="sidebar__stat-title">Population</span>
                 </li>
                 <li className="sidebar__ratio">
                   <span className="sidebar__stat-value">
-                    1 / <CountUp separator={","} end={country.stats.ratio} />
+                    1 / <CountUp delay={1} separator={","} end={country.stats.ratio} />
                   </span>
                   <span className="sidebar__stat-title">Publisher Ratio</span>
                 </li>
@@ -66,7 +70,7 @@ const Sidebar = () => {
                 target="_blank"
                 className="sidebar__jwlink"
               >
-                All information taken from www.jw.org
+                All information taken from www.jw.org. <FontAwesomeIcon className="sidebar__jwlink__icon" icon={faChevronRight} />
               </a>
             </>
           )}
